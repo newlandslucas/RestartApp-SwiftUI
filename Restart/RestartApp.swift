0e3bedec08bb5d128e45832b7ab1db0d2f53bacf
newlandslucas <lucasnewlands.m@gmail.com>
2022-03-24 16:39:10 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Lucas Newlands on 24/03/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
